835e1c9b7450fb32c0a65dd61dd015b903f7699d
GustavAlbert <gustav.gitarrsolo@gmail.com>
2022-11-01 17:45:14 +0100

    List created

--- Labb-3/MainWindow.xaml.cs
+++ Labb-3/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Printing;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,41 @@ namespace Labb_3
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Booking> Bookings { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+
+            Bookings = new List<Booking>();
+        }
+
+        private void btnbook_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnShowBookings_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var Booking in Bookings)
+            {
+
+            }
+        }
+
+        private void btnCancelBooking_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+    public class Booking
+    {
+        public int TableNumber { get; set; }
+        public DateTime Date { get; set; }
+        public Booking (int tablenNumber, DateTime datetime)
+        {
+            TableNumber = tablenNumber;
+            Date = datetime;
         }
     }
 }
